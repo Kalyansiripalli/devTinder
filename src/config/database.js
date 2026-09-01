3238d80970f28fc9efdef686d74dd9cd1f21f0d5
siripalli venkata kalyan <116679580+Kalyansiripalli@users.noreply.github.com>
2026-09-01 13:44:02 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://svk7063_db_user:q_C5WUQH_58Q6_b@personalprojects.cyribyv.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectDB;
+  
